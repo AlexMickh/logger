14141eceaa5b43d82fae987d165e6abea42dcc9c
AlexMickh <sanchespanches33@gmail.com>
2025-03-30 19:19:26 +0300

    add mods

--- pkg/logget/logger.go
+++ pkg/logget/logger.go
@@ -22,10 +22,26 @@ type Logger struct {
 	log *zap.Logger
 }
 
-func New(ctx context.Context) (context.Context, error) {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		return nil, err
+func New(ctx context.Context, env string) (context.Context, error) {
+	var logger *zap.Logger
+	var err error
+
+	switch env {
+	case "prod":
+		logger, err = zap.NewProduction()
+		if err != nil {
+			return nil, err
+		}
+	case "dev":
+		logger, err = zap.NewDevelopment()
+		if err != nil {
+			return nil, err
+		}
+	default:
+		logger, err = zap.NewProduction()
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return context.WithValue(ctx, Key, &Logger{log: logger}), nil
